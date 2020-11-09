f2751fa13eadd043c736481290f61deae26cc850
CHEN Wang <wchen@cse.cuhk.edu.hk>
2020-11-09 16:14:19 +0800

    Update mvrs_config.py

--- mvrs_config.py
+++ mvrs_config.py
@@ -7,7 +7,7 @@ def general_args(parser):
     add some general options
     """
     group = parser.add_argument_group("general_args")
-    group.add('--dataset', '-dataset', type=str, default='tac.10.top12.mds.gen.resp-pyr',
+    group.add('--dataset', '-dataset', type=str, default='tac.2010.psdRef.top12.mds.gen.resp-pyr',
               help="The input data file")
     group.add('--mvrs_type', '-mvrs_type', type=str, default='v2', choices=['v1', 'v2'],
               help="The moverscore version. v1 uses mnli-bert model and v2 uses distilbert-base-uncased model")
